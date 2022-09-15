f0cce808bac9bdb26e21edb401bbdbd6f2b62d7a
Hiago Matheus <oliverhia.math@gmail.com>
2022-09-15 14:48:51 -0300

    Comando para associar função SUPREMA a todas as permissoes

--- app/Domain/Repositories/Tables/FunctionRepository.php
+++ app/Domain/Repositories/Tables/FunctionRepository.php
@@ -4,7 +4,7 @@ namespace App\Domain\Repositories\Tables;
 
 use App\Models\Functions;
 
-class FunctionRepository extends BaseRepository
+class FunctionRepository extends BaseTableRepository
 {
     protected $model = Functions::class;
 }
